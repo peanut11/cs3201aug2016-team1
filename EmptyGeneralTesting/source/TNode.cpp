f78ea4535e3c0c2e07891be0deed5a4f75c89110
Kai Lin <kailin.t@gmail.com>
2016-08-29 11:04:17 +0800

    Define constructors and enum types for TNode

--- EmptyGeneralTesting/source/TNode.cpp
+++ EmptyGeneralTesting/source/TNode.cpp
@@ -8,3 +8,25 @@
 // - DesignExtractor
 
 #include "TNode.h"
+
+TNode::TNode(TNodeType nodeType)
+{
+	mNodeType = nodeType;
+}
+
+
+TNode::TNode(TNodeType nodeType, int nameIndex)
+{
+	mNodeType = nodeType;
+	mNameIndex = nameIndex;
+}
+
+TNode::TNodeType TNode::getNodeType()
+{
+	return mNodeType;
+}
+
+int TNode::getNameIndex()
+{
+	return mNameIndex;
+}
--- EmptyGeneralTesting/source/TNode.h
+++ EmptyGeneralTesting/source/TNode.h
@@ -1,7 +1,21 @@
 // Maintained by: Kai Lin & Ngoc Khanh
+using namespace std;
 
 class TNode
 {
-public:
 
+public:
+	enum TNodeType { nodeTypeProgram, nodeTypeProcedure, nodeTypeStmtLstDefault, 
+		nodeTypeStmtLstThen, nodeTypeStmtLstElse, nodeTypeAssign, nodeTypeWhile, 
+		nodeTypeVariable, nodeTypeConstant, nodeTypePlus };
+
+	TNode(TNodeType nodeType);	
+	// NodeType: program, procedure, variable, constant
+	TNode(TNodeType nodeType, int nameIndex);
+
+	TNodeType getNodeType();
+	int getNameIndex();
+private:
+	TNodeType mNodeType;
+	int mNameIndex;
 };
