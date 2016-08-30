3d57770025c1d987965570b9a45c52af67c6eca9
g4rald <gerald_twk@hotmail.com>
2016-08-31 02:09:19 +0800

    Added header file for Query

--- EmptyGeneralTesting/SPA/QueryTree.h
+++ EmptyGeneralTesting/SPA/QueryTree.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <vector>
+
+class QueryTree {
+
+	// store result synonm from query into a vector
+	std::vector<std::string> result;
+
+	// store relationships query from query into a vector
+	std::vector<std::string> relationship;
+
+	// store the type of results expected (procedure (p) - names, statement(s) - integer, ..)
+	void storeResultType();
+
+	// store the relationships needed to evaluate in the PQL queries (etc. Calls(p,q), Modifies(_,"x"), ......)
+	void storeRelationships();
+
+
+
+public:
+	std::vector<std::string> getResultType();
+	std::vector<std::string> getRelationships();
+
+};
