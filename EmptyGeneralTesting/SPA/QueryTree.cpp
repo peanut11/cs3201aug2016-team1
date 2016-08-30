2be123e21eef8fd722ee7db17f12a3c2aa1c1412
g4rald <gerald_twk@hotmail.com>
2016-08-31 02:06:01 +0800

    Initial structure for Query Model

--- EmptyGeneralTesting/SPA/QueryTree.cpp
+++ EmptyGeneralTesting/SPA/QueryTree.cpp
@@ -5,3 +5,23 @@
 //
 // Used by:
 // - QueryEvaluator
+
+#include "QueryTree.h" 
+
+void QueryTree::storeResultType()
+{
+}
+
+std::vector<std::string> QueryTree::getResultType()
+{
+	return std::vector<std::string>();
+}
+
+std::vector<std::string> QueryTree::getRelationships()
+{
+	return std::vector<std::string>();
+}
+
+void QueryTree::storeRelationships()
+{
+}
