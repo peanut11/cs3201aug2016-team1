85e9285e57c11b5b8c9825a51eb6d7260551b16f
Aaron <aaron.chong13@sps.nus.edu.sg>
2016-09-02 18:10:05 +0800

    Add stubs for ProgramConverter

--- EmptyGeneralTesting/SPA/ProgramConverter.cpp
+++ EmptyGeneralTesting/SPA/ProgramConverter.cpp
@@ -7,9 +7,50 @@
 // - AssignTree
 
 #include "ProgramConverter.h"
-#include "StringTokenizer.h"
 
 ProgramConverter::ProgramConverter() {
-
+	pkb = PKB::getInstance();
 }
 
+void ProgramConverter::convert(std::string program) {
+	StringTokenizer st = StringTokenizer(program, DelimiterMode::PARSER);
+	int numLines = 0;
+
+	for (int lineNum = 1; lineNum < numLines; lineNum++) {
+		std::vector<std::string> currentLine;
+
+		updateStmtInStmtTable(currentLine, lineNum);                 // Aaron
+
+		if (isAssignment(currentLine)) {
+			updateAssignmentInAssignmentTrees(currentLine, lineNum); // Ngoc Khanh
+			updateAssignmentInVarTable(currentLine, lineNum);        // Kai Lin
+		}
+	}
+}
+
+// Aaron
+bool ProgramConverter::isAssignment(std::vector<std::string> line) {
+	return false;
+}
+
+// Ngoc Khanh
+bool ProgramConverter::updateAssignmentInAssignmentTrees(std::vector<std::string> line, int lineNum) {
+	AssignTree tree = AssignTree();
+	pkb->putAssign(lineNum, tree);
+
+	return false;
+}
+
+// Kai Lin
+bool ProgramConverter::updateAssignmentInVarTable(std::vector<std::string> line, int lineNum) {
+	VAR_INDEX var = 0;
+	pkb->putVar(lineNum, MODIFIES, var);
+	pkb->putVar(lineNum, USES, var);
+
+	return false;
+}
+
+// Aaron
+bool ProgramConverter::updateStmtInStmtTable(std::vector<std::string> line, int lineNum) {
+	return false;
+}
--- EmptyGeneralTesting/SPA/ProgramConverter.h
+++ EmptyGeneralTesting/SPA/ProgramConverter.h
@@ -4,8 +4,17 @@
 
 #include "AssignTree.h"
 #include "PKB.h"
+#include "StringTokenizer.h"
 
 class ProgramConverter {
+private:
+	PKB* pkb;
+
 public:
 	ProgramConverter();
+	void convert(std::string);
+	bool isAssignment(std::vector<std::string> line);
+	bool updateAssignmentInAssignmentTrees(std::vector<std::string> line, int lineNum);
+	bool updateAssignmentInVarTable(std::vector<std::string> line, int lineNum);
+	bool updateStmtInStmtTable(std::vector<std::string> line, int lineNum);
 };
