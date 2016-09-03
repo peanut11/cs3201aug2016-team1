934520fee97f8af8a397dec6ce1b5e03d6436938
Aaron <aaron.chong13@sps.nus.edu.sg>
2016-09-03 17:33:42 +0800

    Fix API visibility for ProgramConverter

--- EmptyGeneralTesting/SPA/ProgramConverter.h
+++ EmptyGeneralTesting/SPA/ProgramConverter.h
@@ -10,11 +10,12 @@ class ProgramConverter {
 private:
 	PKB* pkb;
 
-public:
-	ProgramConverter();
-	void convert(std::string source);
 	bool isAssignment(ProgLine line);
 	bool updateAssignmentInAssignmentTrees(ProgLine line, ProgLineNumber lineNum);
 	bool updateAssignmentInVarTable(ProgLine line, ProgLineNumber lineNum);
 	bool updateStmtInStmtTable(ProgLine line, ProgLineNumber lineNum);
+
+public:
+	ProgramConverter();
+	void convert(std::string source);
 };
