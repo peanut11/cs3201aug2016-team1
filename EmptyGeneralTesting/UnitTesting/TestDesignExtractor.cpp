40760fdbb6b844640305dd18a3803ff8d44a5b68
kohchenggee <kohchenggee@gmail.com>
2016-09-09 09:56:50 +0800

    Remove error for TestDesignExtractor

--- EmptyGeneralTesting/UnitTesting/TestDesignExtractor.cpp
+++ EmptyGeneralTesting/UnitTesting/TestDesignExtractor.cpp
@@ -2,31 +2,42 @@
 #include "CppUnitTest.h"
 #include "../SPA/DesignExtractor.h"
 #include "../SPA/ProgramConverter.h"
-
+#include <iostream>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
+using namespace std;
 namespace UnitTesting {
 	TEST_CLASS(TestDesignExtractor) {
 		TEST_CLASS_INITIALIZE(initialize) {
-			std::string str = Tools::readFile("prototype_procedure_Third.txt");
 
-			ProgramConverter pc = ProgramConverter();
-			pc.convert(str);
-			PKB* pkb = PKB::getInstance();
-			
-			DesignExtractor test = DesignExtractor(); 
-			test.process();
-			std::set<StmtNumber> i = pkb->getStmtsByStmt(0, FOLLOWSSTAR);
-			int a = i.size();
-			int expected = 2;
-			bool test = true;
-			if (a == expected) {
-				for (int j = 0; j < a; j++) {
-					
-				}
+		};
+
+public:
+	TEST_METHOD(TestDesign) {
+		std::string str = Tools::readFile("prototype_procedure_Third.txt");
+
+		ProgramConverter pc = ProgramConverter();
+		pc.convert(str);
+		
+
+		DesignExtractor test = DesignExtractor();
+		test.process();
+		PKB* pkb = PKB::getInstance();
+		std::set<StmtNumber> i = pkb->getStmtsByStmt(1, FOLLOWSSTAR);
+		std::set<StmtNumber> k= pkb->getStmtsByStmt(1, FOLLOWS);
+		int a = k.size();
+		int expected = 2;
+		int b = 5;
+		if (a == expected) {
+			for (int j = 0; j < a; j++) {
+
 			}
-			Assert::AreEqual(a, 2);
 		}
+		
+		std::set<VarName> actual;
+		actual = pkb->getAllVarNames();
+		int f = actual.size();
+		Assert::AreEqual(a, 0);
+		Assert::AreEqual(f, 0);
+	}
 	};
-
 }
