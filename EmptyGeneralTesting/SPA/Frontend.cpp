5f158c3a5093b06cacd18cc5352631cf70152d03
Aaron Chong <aaron.chong13@sps.nus.edu.sg>
2016-09-27 19:28:24 +0800

    Update test directory for AutoTester

--- EmptyGeneralTesting/SPA/Frontend.cpp
+++ EmptyGeneralTesting/SPA/Frontend.cpp
@@ -6,7 +6,7 @@
 #include "Frontend.h"
 
 std::string Frontend::read(std::string filename) {
-	std::string foldername = "../Release/"; // Used by AutoTester
+    std::string foldername = ""; // Same directory as AutoTester.exe
 	std::string filepath = foldername.append(filename);
 	std::stringstream buffer;
 	std::ifstream infile(filepath);
