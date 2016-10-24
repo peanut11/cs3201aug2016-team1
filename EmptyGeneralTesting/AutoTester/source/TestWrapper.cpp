9ac3fb53b36c6e98e8a2fb77dda6be4bc0544888
wheel1992 <the_only_light@hotmail.com>
2016-10-24 17:52:55 +0800

    try to implement GlobalStop

--- EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
+++ EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
@@ -28,12 +28,13 @@ void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
 // call your evaluator to evaluate the query here
   // ...code to evaluate query...
 	QueryProcessor *qp = QueryProcessor::getInstance();
-	std::vector<std::string> vector = qp->evaluate(query);
+	//std::vector<std::string> vector = qp->evaluate(query);
 	if (AbstractWrapper::GlobalStop) {
 		//return;
 		throw std::exception("ERROR HERE!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
 	else {
+		std::vector<std::string> vector = qp->evaluate(query);
 		// store the answers to the query in the results list (it is initially empty)
 		// each result must be a string.
 		std::copy(vector.begin(), vector.end(), std::back_inserter(results));
