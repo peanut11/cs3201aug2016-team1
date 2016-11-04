0eb4476276b515161cefd6465d6e36f570ca5ab1
Aaron Chong <aaron.chong13@sps.nus.edu.sg>
2016-11-04 15:50:33 +0800

    Implement select multiple synonyms from different ResultGrids

--- EmptyGeneralTesting/SPA/ResultGridManager.cpp
+++ EmptyGeneralTesting/SPA/ResultGridManager.cpp
@@ -17,6 +17,61 @@ ValueTupleSet ResultGridManager::permutate(ValueSet vals1, ValueSet vals2) {
     return results;
 }
 
+std::list<ValueVector> ResultGridManager::permutateForSynonymTuple(SynonymVector synonyms) {
+    std::list<ValueVector> resultList = { ValueVector(synonyms.size()) };
+
+    std::map<GridIndex, SynonymVector>  gridToSynsMap;
+    std::map<GridIndex, ValueVectorSet> gridToValSetMap;
+    std::map<SynonymString, GridColumn> synToColMap;
+
+    // For each synonym, sort by grids
+    for (size_t col = 0; col < synonyms.size(); col++) {
+        SynonymString syn = synonyms[col];
+        GridIndex id = getGridIndexForSynonym(syn);
+        gridToSynsMap[id].push_back(syn);
+        synToColMap[syn] = col;
+    }
+
+    // For each grid, get the values
+    for (std::map<GridIndex, SynonymVector>::const_iterator it = gridToSynsMap.begin();
+         it != gridToSynsMap.end(); it++) {
+        GridIndex id = it->first;
+        ResultGrid* grid = getGridByIndex(id);
+        gridToValSetMap[id] = grid->getValuesForSynonymTuple(it->second);
+    }
+
+    // For each grid...
+    for (std::map<GridIndex, ValueVectorSet>::const_iterator it = gridToValSetMap.begin();
+         it != gridToValSetMap.end(); it++) {
+        SynonymVector syns = gridToSynsMap[it->first];
+        ValueVectorSet vecSet = it->second;
+
+        // For each row in resultList...
+        for (std::list<ValueVector>::const_iterator row = resultList.begin();
+             row != resultList.end(); row = resultList.erase(row)) {
+
+            // For each ValueVector...
+            for (ValueVectorSet::const_iterator vec = vecSet.begin();
+                 vec != vecSet.end(); vec++) {
+
+                // Keep row as template
+                GridRow newRow = *row;
+
+                // Fill in each value
+                for (size_t i = 0; i < vec->size(); i++) {
+                    GridColumn col = synToColMap[syns[i]];
+                    newRow[col] = (*vec)[i];
+                }
+
+                // Insert newRow before row
+                resultList.insert(row, newRow);
+            }
+        }
+    }
+
+    return resultList;
+}
+
 bool ResultGridManager::createGridForSynonym(SynonymString syn, ValueSet vals) {
     ResultGrid* grid = new ResultGrid(syn, vals);
     refTable.push_back(grid);
@@ -53,6 +108,18 @@ GridIndex ResultGridManager::getGridIndexForSynonym(SynonymString syn) {
 
 bool ResultGridManager::areInSameGrid(SynonymString syn1, SynonymString syn2) {
     return getGridIndexForSynonym(syn1) == getGridIndexForSynonym(syn2);
+}
+
+bool ResultGridManager::areInSameGrid(SynonymVector synonyms) {
+    const GridIndex id = getGridIndexForSynonym(synonyms[0]);
+
+    for (size_t i = 1; i < synonyms.size(); i++) {
+        if (id != getGridIndexForSynonym(synonyms[i])) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 bool ResultGridManager::initialiseSynonym(SynonymString syn, ValueSet vals) {
@@ -112,7 +179,15 @@ ValueTupleSet ResultGridManager::getValuesForSynonymTuple(SynonymTuple synTuple)
 }
 
 ValueVectorSet ResultGridManager::getValuesForSynonymTuple(SynonymVector synonyms) {
-    // Currently assumes all synonyms from same ResultGrid
-    ResultGrid* grid = getGridForSynonym(synonyms[0]);
-    return grid->getValuesForSynonymTuple(synonyms);
+    ValueVectorSet result;
+
+    if (areInSameGrid(synonyms)) {
+        ResultGrid* grid = getGridForSynonym(synonyms[0]);
+        result = grid->getValuesForSynonymTuple(synonyms);
+    } else {
+        std::list<ValueVector> resultList = permutateForSynonymTuple(synonyms);
+        result = ValueVectorSet(resultList.begin(), resultList.end());
+    }
+
+    return result;
 }
